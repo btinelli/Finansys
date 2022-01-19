6e5eac1ffeac91b923435b8c505eec902d747079
Bruno Tinelli <bruno.tinelli@gmail.com>
2022-01-19 17:10:00 -0300

    Adição do formulario de lançamento

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
+import { EntryFormComponent } from './entry-form/entry-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [EntryListComponent],
+  declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class EntriesModule { }
