c0ab833600024fcc6660e19c51099bd4714903ff
Bruno Tinelli <bruno.tinelli@gmail.com>
2022-01-19 16:54:30 -0300

    Criação da listagem de lançamentos

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Observable } from 'rxjs';
 import { throwError } from 'rxjs';
 import { Entry } from './entry.model';
@@ -48,12 +49,15 @@ export class EntryService {
 
   private jsonDataToEntryes(jsonData: any[]): Entry[] {
     const entries: Entry[] = []
-    jsonData.forEach(element => entries.push(element as Entry))
+    jsonData.forEach(element => {
+      const entry = Object.assign(new Entry(), element)
+      entries.push(entry)
+    })
     return entries
   }
 
   private jsonDataToEntry(jsonData: any): Entry {
-    return jsonData as Entry
+    return Object.assign(new Entry(), jsonData)
   }
 
   private handleError(error: any): Observable<any> {
